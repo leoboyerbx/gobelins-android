eebd90c61a7c6e6eb5a863bd7379d8e30df4bdfd
leoboyerbx <contact@leoboyer.fr>
2021-03-31 11:37:50 +0200

    Avant app 2

--- BDDI/app/src/main/java/com/pnk/gobelins/bddi/MainActivity.kt
+++ BDDI/app/src/main/java/com/pnk/gobelins/bddi/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.pnk.gobelins.bddi
 
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.Animation.AnimationListener
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.pnk.gobelins.bddi.databinding.ActivityMain2Binding
+import com.pnk.gobelins.bddi.databinding.ActivityMainBinding
 
 /**
  * Activity enabling the user to roll a dice and see the result.
@@ -17,54 +13,58 @@ class MainActivity : AppCompatActivity() {
      * lateinit permet de garantir au compilateur qu'on va initialiser la variable dès que possible.
      * Si on oublie de l'initialiser, l'app va planter.
      */
-    lateinit var binding: ActivityMain2Binding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main_2)
-        binding.myButton.setOnClickListener { rollAllDices() }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
 
-        rollAllDices()
+        val dataChange = DataChange()
+        binding.nbClick = dataChange
+        binding.myButton.setOnClickListener {
+            dataChange.newData()
+        }
     }
 
     /**
      * Roll a 6-faces dice and update the result in the view for the specified imageView
      */
-    private fun rollDice(diceImage: ImageView) {
-        // Create a dice and roll it
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        // Determine which drawable resource ID to use based on the dice roll
-        val diceImageResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        val anim: Animation = AnimationUtils.loadAnimation(this, R.anim.shake)
-        anim.setAnimationListener(object : AnimationListener {
-            override fun onAnimationStart(animation: Animation?) {
-                // nothing to do here
-            }
-
-            override fun onAnimationEnd(animation: Animation?) {
-                diceImage.setImageResource(diceImageResource)
-                diceImage.contentDescription = diceRoll.toString()
-            }
-
-            override fun onAnimationRepeat(animation: Animation?) {
-                // nothing to do here
-            }
-        })
-        diceImage.startAnimation(anim)
-    }
-
-    private fun rollAllDices() {
-        val diceImage1: ImageView = binding.imageView
-        val diceImage2: ImageView = binding.imageView2
-        rollDice(diceImage1)
-        rollDice(diceImage2)
-    }
+//    private fun rollDice(diceImage: ImageView) {
+//        // Create a dice and roll it
+//        val dice = Dice(6)
+//        val diceRoll = dice.roll()
+//
+//        // Determine which drawable resource ID to use based on the dice roll
+//        val diceImageResource = when (diceRoll) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+//        val anim: Animation = AnimationUtils.loadAnimation(this, R.anim.shake)
+//        anim.setAnimationListener(object : AnimationListener {
+//            override fun onAnimationStart(animation: Animation?) {
+//                // nothing to do here
+//            }
+//
+//            override fun onAnimationEnd(animation: Animation?) {
+//                diceImage.setImageResource(diceImageResource)
+//                diceImage.contentDescription = diceRoll.toString()
+//            }
+//
+//            override fun onAnimationRepeat(animation: Animation?) {
+//                // nothing to do here
+//            }
+//        })
+//        diceImage.startAnimation(anim)
+//    }
+//
+//    private fun rollAllDices() {
+//        val diceImage1: ImageView = binding.imageView
+//        val diceImage2: ImageView = binding.imageView2
+//        rollDice(diceImage1)
+//        rollDice(diceImage2)
+//    }
 }
